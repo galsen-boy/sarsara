663df9b3fb1e197c5e108a9efaff6913949b4553
galsen-boy <badaibou5@gmail.com>
2025-10-15 19:10:29 +0000

    Replace all blue colors with custom SARSARA color #334692

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'sarsara': {
+          50: '#f0f2ff',
+          100: '#e6ebff',
+          400: '#334692',
+          500: '#334692',
+          600: '#334692',
+          700: '#334692',
+          800: '#334692',
+          900: '#334692',
+        }
+      },
+    },
   },
   plugins: [],
 };
